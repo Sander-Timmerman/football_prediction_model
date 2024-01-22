e5d9be7d72f431ae079cabeac75fa3f9b925ab5c
Sander-Timmerman <sander.timmerman@alliander.com>
2024-01-22 15:52:31 +0100

    solve warnings

--- R/parse_date_from_transfermarkt.R
+++ R/parse_date_from_transfermarkt.R
@@ -1,15 +1,20 @@
 parse_date_from_transfermarkt <- function(date_string) {
+  date_string[date_string == "-"] <- "Jan 1, 1900"
   jaar <- as.numeric(substr(date_string, nchar(date_string) - 3, nchar(date_string)))
-  jaar[is.na(jaar)] <- 1900
   
   maand <- substr(date_string, 1, 3)
   maanden <- read.csv("input/maanden.csv", colClasses = c(rep("character", 2)))
   maand <- mgsub(maand, maanden$Maand, maanden$Cijfer)
-  maand[is.na(maand)] <- 1
   
   dag <- as.numeric(substr(date_string, 5, nchar(date_string) - 6))
-  dag[is.na(dag)] <- 1
   
-  date <- as.Date(paste(jaar, maand, dag, sep="-"))
+  if(any(is.na(jaar)) | any(is.na(maand)) | any(is.na(dag))) {
+    flog.warn("Unknown date format in Transfermarkt data. Giving those a default of 1 January 1900")
+    jaar[is.na(jaar)] <- 1900
+    maand[is.na(maand)] <- 1
+    dag[is.na(dag)] <- 1
+  }
+  date <- as.Date(paste(jaar, maand, dag, sep = "-"))
+  
   return(date)
 } 
