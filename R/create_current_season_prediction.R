bf3752bc310b1d59a4c8617df244d67cfdd3ceaf
Sander-Timmerman <sander.timmerman@alliander.com>
2024-08-02 18:27:18 +0200

    handle some exceptional cases

--- R/create_current_season_prediction.R
+++ R/create_current_season_prediction.R
@@ -2,9 +2,12 @@ create_current_season_prediction <- function(aggregated_football_data, aggregate
   flog.info("Using models to make a prediction for current season")
   
   aggregated_football_data_with_shots <- aggregated_football_data[!is.na(aggregated_football_data$Schotsaldo), ]
-  aggregated_football_data_without_shots <- aggregated_football_data[is.na(aggregated_football_data$Schotsaldo), ]
-  browser()
-  aggregated_level_two_data <- left_join(aggregated_level_two_data, aggregated_football_data_without_shots, by = c("Team", "Competitie", "Seizoen"))
+  aggregated_football_data_without_shots <- aggregated_football_data[is.na(aggregated_football_data$Schotsaldo), ] %>%
+    mutate(Doelpuntenvoor = NA,
+           Doelpuntentegen = NA) %>%
+    select(Team, Competitie, Seizoen, Aantalwedstrijden, Punten, Doelpuntenvoor, Doelpuntentegen, Niveau, Doelsaldo, Goalratio, Doelsom)
+  aggregated_level_two_data <- aggregated_level_two_data %>%
+    rbind(aggregated_football_data_without_shots)
   
   prediction_with_shots <- use_model_on_new_data(aggregated_football_data_with_shots, 
                                                  aggregated_transfermarkt_data_new, 
