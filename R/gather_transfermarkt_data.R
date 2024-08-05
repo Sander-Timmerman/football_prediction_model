b60e4acefcaead20bb949ddb5f278362d4596dc5
Sander-Timmerman <sander.timmerman@alliander.com>
2024-08-05 23:28:54 +0200

    add debug log

--- R/gather_transfermarkt_data.R
+++ R/gather_transfermarkt_data.R
@@ -23,6 +23,7 @@ gather_transfermarkt_data <- function(urls_tm, player_jsons = list(), is_current
                                object_name = "player_jsons")
       club_name <- html_nodes(webpage_club, ".data-header__headline-wrapper--oswald") %>% html_text()
       club_name <- substr(club_name, 14, nchar(club_name) - 8)
+      flog.debug(paste0("Collecting data for club ", club_name))
       player_names <- html_nodes(webpage_club, ".inline-table a") %>% html_text()
       player_urls <- html_nodes(webpage_club, ".inline-table a") %>% html_attr("href")
       slashes <- unlist(gregexpr("/", player_urls, fixed = TRUE))[seq(4, length(player_urls) * 4, 4)]
--- use_prediction_application.R
+++ use_prediction_application.R
@@ -8,14 +8,12 @@ library(xlsx)
 
 rm(list = ls())
 options(dplyr.summarise.inform = FALSE)
-flog.threshold(TRACE)
+flog.threshold(INFO)
 
 source.all("R")
 
-output <- run_prediction_application(football_data_cache = 8,
-                                     aggregated_football_data_cache = 8,
-                                     n_sims = 1,
+output <- run_prediction_application(n_sims = 10000,
                                      write_results = TRUE)
-# View(output$results_table)
-# View(output$prediction)
-# View(output$next_game_round_prediction)
+View(output$results_table)
+View(output$prediction)
+View(output$next_game_round_prediction)
