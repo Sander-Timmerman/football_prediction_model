22d92d4afcfda66256d2e45f798d29001177883b
Sander-Timmerman <sander.timmerman@alliander.com>
2024-08-21 01:16:52 +0200

    add spanner for points per position

--- R/save_results_table_as_html.R
+++ R/save_results_table_as_html.R
@@ -1,4 +1,4 @@
-save_results_table_as_html <- function(results_table, file_path) {
+save_results_table_as_html <- function(results_table, points_per_position, file_path) {
   gt_table <- results_table %>%
     gt() %>%
     fmt_number(columns = c(Positie, Punten), decimals = 1, sep_mark = "", dec_mark = ",") %>%
@@ -6,12 +6,12 @@ save_results_table_as_html <- function(results_table, file_path) {
     fmt_percent(columns = colnames(results_table)[5 : ncol(results_table)], 
                 decimals = 0, 
                 scale_values = TRUE) %>%
-    cols_align(align = "center", columns = c(colnames(results_table)[-1])) %>% 
+    cols_align(align = "center", columns = colnames(results_table)[-1]) %>% 
     tab_style(style = cell_text(weight = "bold"), locations = cells_column_labels(columns = everything())) %>%
-    tab_options(table.font.size = "small", column_labels.font.size = "medium", table.align = "left") %>%
-    tab_spanner(
-      label = "Probabilities",
-      columns = colnames(results_table)[5 : ncol(results_table)]
-    )
+    tab_options(table.font.size = "small", column_labels.font.size = "medium", table.align = "left")
+  for(position in seq_along(points_per_position)) {
+    gt_table <- gt_table %>%
+      tab_spanner(label = points_per_position[position], columns = colnames(results_table)[position + 4])
+  }
   gtsave(gt_table, file_path)
 }
