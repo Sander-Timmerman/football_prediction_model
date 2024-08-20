7d15ff7fec0ebfd21a5799799fb5cac794ccb87e
Sander-Timmerman <sander.timmerman@alliander.com>
2024-08-21 01:16:12 +0200

    bug fix

--- R/create_results_table.R
+++ R/create_results_table.R
@@ -1,11 +1,10 @@
 create_results_table <- function(all_simulations, n_sims, prediction_competition) {
-  results_table <- as.data.frame.matrix(table(as.character(all_simulations$Team), all_simulations$Rank)) / n_sims
+  results_table <- as.data.frame.matrix(table(factor(all_simulations$Team, levels = prediction_competition$Team), all_simulations$Rank)) / n_sims
   results_table <- all_simulations %>%
     group_by(Team) %>%
     summarise(Positie = mean(Rank),
               Punten = mean(Punten)) %>%
     mutate(Rating = prediction_competition$Punten) %>%
-    mutate(Team = sort(Team)) %>%
     cbind(results_table) %>%
     arrange(Positie)
   rownames(results_table) <- NULL
--- R/do_monte_carlo_simulation.R
+++ R/do_monte_carlo_simulation.R
@@ -9,7 +9,7 @@ do_monte_carlo_simulation <- function(prediction, football_data_new, namen, sett
         prediction_competition <- prediction %>%
           filter(Competitie == competition) %>%
           arrange(Team)
-        all_teams <- sort(unique(prediction_competition$Team))
+        all_teams <- pull(arrange(data.frame(Team = unique(prediction_competition$Team))), Team)
         n_teams <- length(all_teams)
         
         if(nrow(football_data_new) > 0) {
@@ -72,7 +72,12 @@ do_monte_carlo_simulation <- function(prediction, football_data_new, namen, sett
         
         if(settings$write_results) {
           write.xlsx(results_table, file.path("output", run_number, paste0("results_table_", competition, ".xlsx")))
-          save_results_table_as_html(results_table, file.path("output", run_number, paste0("results_table_", competition, ".html")))
+          
+          points_per_position <- all_simulations %>% 
+            group_by(Rank) %>% 
+            summarise(Punten = round(mean(Punten), 1)) %>% 
+            pull(Punten)
+          save_results_table_as_html(results_table, points_per_position, file.path("output", run_number, paste0("results_table_", competition, ".html")))
           flog.info(paste0("Written results table for competition ", competition, " in the output folder"))
         }
         results_table
