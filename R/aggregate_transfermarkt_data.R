e05839c0ebc519c44090104f5af6a0d77cb2d56d
Sander-Timmerman <sander.timmerman@alliander.com>
2024-09-27 11:48:44 +0200

    changed club name on Transfermarkt

--- R/aggregate_transfermarkt_data.R
+++ R/aggregate_transfermarkt_data.R
@@ -2,6 +2,7 @@ aggregate_transfermarkt_data <- function(transfermarkt_data) {
   flog.info("Starts aggregating data from transfermarkt")
   aggregated_transfermarkt_data <- transfermarkt_data %>%
     mutate(Team = ifelse(Team == "CF Os Belenenses", "B SAD", as.character(Team)),
+           Team = ifelse(Team == "GD Estoril Praia", "Estoril Praia", as.character(Team)),
            Marktwaarde = Marktwaarde ^ (1 / 3)) %>%
     arrange(desc(Marktwaarde)) %>%
     group_by(Team, Competitie, Seizoen) %>%
