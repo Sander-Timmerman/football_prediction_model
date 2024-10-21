9b5952e8a4ccb824ae4fc4fe4709c46d3f8b8996
Sander-Timmerman <sander.timmerman@alliander.com>
2024-10-21 14:36:32 +0200

    delete step that's already done in a different function

--- R/add_additional_football_data.R
+++ R/add_additional_football_data.R
@@ -11,7 +11,6 @@ add_additional_football_data <- function(df_football_data, additional_football_d
            AS = coalesce(AS.y, AS.x),
            HST = coalesce(HST.y, HST.x),
            AST = coalesce(AST.y, AST.x)) %>%
-    select(Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR, HS, AS, HST, AST) %>%
-    arrange(as.Date(Date))
+    select(Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR, HS, AS, HST, AST)
   return(df_football_data)
 }
