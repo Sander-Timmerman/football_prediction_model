6e1c2bf38abef33e6640ce75fa9f7fb5aea61311
Sander-Timmerman <sander.timmerman@alliander.com>
2025-04-03 13:34:06 +0200

    without parameters

--- use_prediction_application.R
+++ use_prediction_application.R
@@ -14,13 +14,4 @@ flog.threshold(INFO)
 
 source.all("R")
 
-output <- run_prediction_application(football_data_cache = 649,
-                                     aggregated_football_data_cache = 651,
-                                     player_jsons_cache = 51,
-                                     transfermarkt_data_cache = 51,
-                                     transfermarkt_data_new_cache = 771,
-                                     all_final_standings_cache = 273,
-                                     all_models_cache = 704, #297,
-                                     n_sims = 1,
-                                     write_results = TRUE,
-                                     edit_blogger = TRUE)
+output <- run_prediction_application()
