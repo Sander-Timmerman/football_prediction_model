ac31d4407901bb338fed5a91d608dc2e5c5243dd
Sander-Timmerman <sander.timmerman@alliander.com>
2025-04-11 20:28:49 +0200

    center last column also

--- R/save_match_expectations_as_html.R
+++ R/save_match_expectations_as_html.R
@@ -3,7 +3,7 @@ save_match_expectations_as_html <- function(match_expectations, blogger_info, ed
     gt() %>%
     fmt_number(columns = c(Thuisgoals, Uitgoals), decimals = 2, sep_mark = ".", dec_mark = ",") %>%
     fmt_percent(columns = c(Thuiswinst, Gelijk, Uitwinst), decimals = 0) %>%
-    cols_align(align = "center", columns = colnames(match_expectations)[3 : 8]) %>% 
+    cols_align(align = "center", columns = colnames(match_expectations)[3 : 9]) %>% 
     tab_style(style = cell_text(weight = "bold"), locations = cells_column_labels(columns = everything())) %>%
     tab_options(table.font.size = "small", column_labels.font.size = "medium", table.align = "left") %>%
     {if(edit_blogger) edit_blogger_page(., blogger_info, "Wedstrijdkansen") else .} %>%
