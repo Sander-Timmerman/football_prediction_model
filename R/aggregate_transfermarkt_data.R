e8a56971e4b8fbb3697846f239dee73555368600
Sander-Timmerman <sander.timmerman@alliander.com>
2025-07-25 20:59:33 +0200

    tweak parameters

--- R/aggregate_transfermarkt_data.R
+++ R/aggregate_transfermarkt_data.R
@@ -1,15 +1,15 @@
-aggregate_transfermarkt_data <- function(transfermarkt_data) {
+aggregate_transfermarkt_data <- function(transfermarkt_data, market_value_adjustment = 2.8, n_players = 12, regularisation_param = 2.15) {
   flog.debug("Starts aggregating data from transfermarkt")
   aggregated_transfermarkt_data <- transfermarkt_data %>%
     mutate(Team = ifelse(Team == "CF Os Belenenses", "B SAD", as.character(Team)),
-           Marktwaarde = Marktwaarde ^ (1 / 3)) %>%
+           Marktwaarde = Marktwaarde ^ (1 / market_value_adjustment)) %>%
     arrange(desc(Marktwaarde)) %>%
     group_by(Team, Competitie, Seizoen) %>%
-    summarise(Marktwaarde = sum(Marktwaarde[1 : min(n(), 20)])) %>%
+    summarise(Marktwaarde = sum(Marktwaarde[1 : min(n(), n_players)])) %>%
     ungroup() %>%
     group_by(Competitie, Seizoen) %>%
     mutate(Marktwaarde = Marktwaarde / mean(Marktwaarde),
-           Standaardafwijking = (sd(Marktwaarde) * 1.23727 * 0.4277 + 0.2138) / (sd(Marktwaarde) * 1.23727),
+           Standaardafwijking = regularisation_param + 1 / sd(Marktwaarde),
            Marktwaarde = (Marktwaarde - 1) * Standaardafwijking + 1) %>%
     ungroup() %>%
     select(-Standaardafwijking)
